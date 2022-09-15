75b2eb934a1c666f568df6f0e7e46094f583bee8
Mikael Allison <mikellallison@gmail.com>
2022-09-15 15:42:16 +0100

    Add rentention to logs group, mod userdat for easier install of sqlplus, modifiy secret names from name to name_prefix

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,3 +1,4 @@
 resource "aws_cloudwatch_log_group" "rds_proxy_log_group" {
-  name = "/aws/rds/proxy/${var.name}"
+  name              = "/aws/rds/proxy/${aws_db_proxy.rds_proxy.id}"
+  retention_in_days = 1
 }
